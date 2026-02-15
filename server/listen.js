0e6b6ac2e053c902629eb7fcc889fbba0b5d5f8e
ofeore <ofeore@gmail.com>
2026-02-15 21:28:57 +0000

    Update console log message for server running

--- server/listen.js
+++ server/listen.js
@@ -1,4 +1,6 @@
 const app = require("./app");
 
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Server running on http://localhost:${PORT}`),
+);
